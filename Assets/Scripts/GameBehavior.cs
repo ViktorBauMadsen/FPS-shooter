3464186ef9f93531164325632325b69e3e5782e7
ViktorBauMadsen <viktorbaumadsen@gmail.com>
2025-02-20 16:13:45 +0100

    Initial commit

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using CustomExtensions;
+
+public class GameBehavior : MonoBehaviour, IManager
+{
+    public int MaxItems = 4;
+    public Text HealthText;
+    public Text ItemText;
+    public Text ProgressText;
+    public Button WinButton;
+    public Button LossButton;
+    private string _state;
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    void Start()
+    {
+        Initialize();
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
+    }
+
+    public void Initialize()
+    {
+        _state = "Game Manager initialized..";
+        _state.FancyDebug();
+        Debug.Log(_state);
+        
+    }
+    private int _itemsCollected = 0;
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+            ItemText.text = "Items Collected: " + Items;
+
+            if (_itemsCollected >= MaxItems)
+            {
+                WinButton.gameObject.SetActive(true);
+                UpdateScene("You've found all the items!");
+            }
+            else
+            {
+                ProgressText.text = "Item found, only " + (MaxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+
+    private int _playerHP = 10;
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            HealthText.text = "Player Health: " + HP;
+
+            if (_playerHP <= 0)
+            {
+                LossButton.gameObject.SetActive(true);
+                UpdateScene("You want another life with that?");
+            }
+            else
+            {
+                ProgressText.text = "Ouch... that's got hurt.";
+            }
+
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+    public void UpdateScene(string updatedText)
+    {
+        ProgressText.text = updatedText;
+        Time.timeScale = 0f;
+    }
+
+    public void RestartScene()
+    {
+        Utilities.RestartLevel(0);
+    }
+}
